4292a8461799b62eca35e8e566e363eb61875630
Sidney Ribeiro Junior <srjsoftware@gmail.com>
2016-03-30 17:03:09 -0300

    Modificando o main para voltar a suar dois arquivos

--- main.cu
+++ main.cu
@@ -54,7 +54,10 @@ struct FileStats {
 	FileStats() : num_docs(0), num_terms(0) {}
 };
 
-FileStats readInputFile(string &file, vector<Entry> &entries, vector<string>& inputs);
+FileStats readInput1(string &file, vector<Entry> &entries);
+void readInput2(string &filename, vector<string>& inputs);
+void updateStatsMaxFeatureTest(FileStats &stats, vector<string>& inputs);
+
 void processTestFile(InvertedIndex &index, FileStats &stats, vector<string>& input,
 	string &file, float threshold, string distance, stringstream &fileout);
 bool makeQuery(InvertedIndex &inverted_index, FileStats &stats, string &line, float threshold, 
@@ -76,16 +79,16 @@ int biggestQuerySize = -1;
 
 int main(int argc, char **argv) {
 
-	if (argc != 6) {
-		cerr << "Wrong parameters. Correct usage: <executable> <input_file> <threshold> <cosine | l2 | l1> <output_file> <number_of_gpus>" << endl;
+	if (argc != 7) {
+		cerr << "Wrong parameters. Correct usage: <executable> <input_file1> <input_file2> <threshold> <cosine | l2 | l1> <output_file> <number_of_gpus>" << endl;
 		exit(1);
 	}
 
 	int gpuNum;
 	cudaGetDeviceCount(&gpuNum);
 
-	if (gpuNum > atoi(argv[5])){
-		gpuNum = atoi(argv[5]);
+	if (gpuNum > atoi(argv[6])){
+		gpuNum = atoi(argv[6]);
 		if (gpuNum < 1)
 			gpuNum = 1;
 	}
@@ -98,10 +101,10 @@ int main(int argc, char **argv) {
 
 #if OUTPUT
 	//truncate output files
-	ofstream ofsf(argv[4], ofstream::trunc);
+	ofstream ofsf(argv[5], ofstream::trunc);
 	ofsf.close();
 
-	ofstream ofsfileoutput(argv[4], ofstream::out | ofstream::app);
+	ofstream ofsfileoutput(argv[5], ofstream::out | ofstream::app);
 #endif
 	vector<string> inputs;// to read the whole test file in memory
 	vector<InvertedIndex> indexes;
@@ -109,16 +112,19 @@ int main(int argc, char **argv) {
 
 	double starts, ends;
 
-	string inputFileName(argv[1]);
+	string input1FileName(argv[1]);
+	string input2FileName(argv[2]);
 
-	printf("Reading file...\n");
+	printf("Reading files...\n");
 	vector<Entry> entries;
 
 	starts = gettime();
-	FileStats stats = readInputFile(inputFileName, entries, inputs);
+	FileStats stats = readInput1(input1FileName, entries);
+	readInput2(input2FileName, inputs);
+	updateStatsMaxFeatureTest(stats, inputs);
 	ends = gettime();
 
-	printf("time taken: %lf seconds\n", ends - starts);
+	printf("Time taken: %lf seconds\n", ends - starts);
 	//fprintf(stderr,"sizeof Entry %u , sizeof Similarity %u\n",sizeof(Entry), sizeof(Similarity));
 
 	vector<stringstream*> outputString;
@@ -148,12 +154,12 @@ int main(int argc, char **argv) {
 		int cpuid = omp_get_thread_num();
 		cudaSetDevice(cpuid / NUM_STREAMS);
 
-		float threshold = atof(argv[2]);
-		string distanceFunction(argv[3]);
+		float threshold = atof(argv[3]);
+		string distanceFunction(argv[4]);
 
 		FileStats lstats = stats;
 
-		processTestFile(indexes[cpuid / NUM_STREAMS], lstats, inputs, inputFileName, threshold,
+		processTestFile(indexes[cpuid / NUM_STREAMS], lstats, inputs, input2FileName, threshold,
 			distanceFunction, *outputString[cpuid]);
 		if (cpuid %  NUM_STREAMS == 0)
 			gpuAssert(cudaDeviceReset());
@@ -167,14 +173,14 @@ int main(int argc, char **argv) {
 		}
 		ends = gettime();
 
-		printf("time taken to write output: %lf seconds\n", ends - starts);
+		printf("Time taken to write output: %lf seconds\n", ends - starts);
 
 		ofsfileoutput.close();
 #endif
 		return 0;
 }
 
-FileStats readInputFile(string &filename, vector<Entry> &entries, vector<string>& inputs) {
+FileStats readInput1(string &filename, vector<Entry> &entries) {
 	ifstream input(filename.c_str());
 	string line;
 
@@ -184,17 +190,8 @@ FileStats readInputFile(string &filename, vector<Entry> &entries, vector<string>
 		getline(input, line);
 		if (line == "") continue;
 
-		inputs.push_back(line);
-		num_tests++;
-	}
-	
-	stats.num_docs = num_tests;
-	
-	for (int doc_id = 0; doc_id < num_tests; doc_id++) {
-		line = inputs[doc_id];
-		
+		int doc_id = stats.num_docs++;
 		vector<string> tokens = split(line, ' ');
-		biggestQuerySize = max((int)tokens.size() / 2, biggestQuerySize);
 
 		stats.doc_to_class[doc_id] = get_class(tokens[1]);
 
@@ -211,23 +208,69 @@ FileStats readInputFile(string &filename, vector<Entry> &entries, vector<string>
 	return stats;
 }
 
+void readInput2(string &filename, vector<string>& inputs) {
+	ifstream input(filename.c_str());
+	string line;
+
+	while (!input.eof()) {
+		getline(input, line);
+		if (line == "") continue;
+		num_tests++;
+		inputs.push_back(line);
+	}
+}
+
+void updateStatsMaxFeatureTest(FileStats &stats, vector<string>& inputs) {
+
+	#pragma omp parallel num_threads(4)
+   {
+	   int localBiggest = -1;
+	   int localNumterms = stats.num_terms;
+		#pragma omp for
+	   for (int i = 0; i < inputs.size(); i++) {
+
+		   vector<string> tokens = split(inputs[i], ' ');
+
+		   localBiggest = max((int)tokens.size() / 2, localBiggest);
+		   for (int i = 2, size = tokens.size(); i + 1 < size; i += 2) {
+			   int term_id = atoi(tokens[i].c_str());
+			   localNumterms = max(localNumterms, term_id + 1);
+		   }
+	   }
+
+		#pragma omp critical
+		{
+			stats.num_terms = max(localNumterms, stats.num_terms);
+			biggestQuerySize = max(biggestQuerySize, localBiggest);
+		}
+
+   }
+}
+
 void allocVariables(DeviceVariables *dev_vars, float threshold, int num_docs, Similarity** distances){
+
+	//int KK = 1;    //Obtain the smallest power of 2 greater than K (facilitates the sorting algorithm)
+	//while (KK < K) KK <<= 1;
+
 	dim3 grid, threads;
 
 	get_grid_config(grid, threads);
 
 	gpuAssert(cudaMalloc(&dev_vars->d_dist, num_docs * sizeof(Similarity)));
+	//gpuAssert(cudaMalloc(&dev_vars->d_nearestK, KK * grid.x * sizeof(Similarity)));
 	gpuAssert(cudaMalloc(&dev_vars->d_query, biggestQuerySize * sizeof(Entry)));
 	gpuAssert(cudaMalloc(&dev_vars->d_index, biggestQuerySize * sizeof(int)));
 	gpuAssert(cudaMalloc(&dev_vars->d_count, biggestQuerySize * sizeof(int)));
 	gpuAssert(cudaMalloc(&dev_vars->d_qnorms, 2 * sizeof(float)));
 
+	//*k_nearest = (Similarity*)malloc(KK * grid.x * sizeof(Similarity));
 	*distances = (Similarity*)malloc(num_docs * sizeof(Similarity));
 
 }
 
 void freeVariables(DeviceVariables *dev_vars, InvertedIndex &index, Similarity** distances){
 	cudaFree(dev_vars->d_dist);
+	//cudaFree(dev_vars->d_nearestK);
 	cudaFree(dev_vars->d_query);
 	cudaFree(dev_vars->d_index);
 	cudaFree(dev_vars->d_count);
@@ -345,6 +388,7 @@ bool makeQuery(InvertedIndex &inverted_index, FileStats &stats, string &line, fl
 	vector<Entry> query;
 	vector<string> tokens = split(line, ' ');
 
+	int trueclass = get_class(tokens[1]);
 	int docid = atoi(tokens[0].c_str());
 
 	for (int i = 2, size = tokens.size(); i + 1 < size; i += 2) {
